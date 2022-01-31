e4abb9d2daaf93b93ec06047b5f3e5d924e0c0f0
lcanavesio <lcanavesio@gmail.com>
2022-01-31 17:05:22 -0300

    feat: add gtag & search console

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,21 @@ export default class MyDocument extends Document {
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           ></script>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-168027767-1"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-168027767-1', { page_path: window.location.pathname });
+            `,
+            }}
+          />
+          <meta name="google-site-verification" content="u8B_jRPxV9QEkABtRYRMUkTb8CuYZ8I29fPWnSzKEQ8" />
         </Head>
         <body>
           <Main />
